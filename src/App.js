57be8ff0dbc1ab4bb6603e0c497dd0d490bb50c4
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-01-19 14:37:53 +0400

    Add redux and setup store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import Currency from './components/Currency'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Currency />
+  </Provider>
+)
 
-export default App;
+export default App
